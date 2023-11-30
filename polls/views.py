b9a1de1c182f8cb764acd4b5b0513075f53c4ac3
Jack Ryan <jack@canopyteam.org>
2023-11-30 13:43:42 -0600

    finished part 3

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,26 @@
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Question
+
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list,}
+    return render(request, "polls/index.html", context)
+
+
+
+#I could be wrong but this %s seems outdated. I think an f-string would be better
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html",  {"question": question})
+
+def results(request, question_id):
+    response = "You're looking at the result of questions %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." %question_id)
